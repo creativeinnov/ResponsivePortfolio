1c1ccc599ceb656190566050a5029a274fad1437
creativeinnov <mehtalaxmi1234@gmail.com>
2024-12-30 15:03:04 +0530

    updated files as wholely completed

--- portfolio.js
+++ portfolio.js
@@ -19,3 +19,15 @@ const menuToggle = document.querySelector('.menu-toggle');
         backDelay: 1000,
         loop: true
       });
+
+      const scriptURL = 'https://script.google.com/macros/s/AKfycbwVfQy_xDgXgb_rA4l95xIkYZ3S8I2cSHt8KUekGz9CtziPnrZjr2sm0t3B8Ty1cao-/exec'
+
+      const form = document.forms['contact-forms']
+      
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+        .then(response => alert("Thank you! Your Form is Submitted Successfully."))
+        .then(() => {window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+      })
